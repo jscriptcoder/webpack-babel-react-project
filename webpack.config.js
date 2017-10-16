46abab3066ff9129b8b630644e17f7ea1c95910f
Fran <jscriptcoder@gmail.com>
2017-10-16 10:24:25 +0200

    Added babel configuration

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,18 @@ module.exports = {
     })
   ], 
 
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+    }]  
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+
   devServer: {
     contentBase: paths.SRC
   }
